ccd016e0a8e4b549a4bf907a243dedb9e199d4ef
ASchwartzman <aleschwartzman22@gmail.com>
2018-03-21 23:18:53 -0300

    Random Forest 5d aprovado como um bom indicador

---
+++ Modelo.R
@@ -0,0 +1,8 @@
+setwd("/Users/aleschwartzman/Documents/Big Data/Modelo BRL") #estabelecendo o diretório de trab
+#set.seed(3)
+source('Config.R')
+#source('BBG download.R)
+source('Charts.R')
+source('Data Structure.R')
+source('Random Forest m5.R')
+
